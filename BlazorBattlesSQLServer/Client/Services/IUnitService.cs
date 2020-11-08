c4f858ce34f28893a719b6ebfb973f18c1bf304a
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-08 12:24:33 +0100

    New Models for Knights & More

---
+++ BlazorBattlesSQLServer/Client/Services/IUnitService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorBattlesSQLServer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace BlazorBattlesSQLServer.Client.Services
+{
+    public interface IUnitService
+    {
+        IList<Unit> Units { get; }
+        IList<UserUnit> MyUnits { get; set; }
+        void AddUnit(int unitId);
+    }
+}
---
+++ BlazorBattlesSQLServer/Shared/UserUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBattlesSQLServer.Shared
+{
+
+    public class UserUnit
+    {
+        public int UserId { get; set; }
+        public int UnitId { get; set; }
+        public int HitPoints { get; set; }
+    }
+}
