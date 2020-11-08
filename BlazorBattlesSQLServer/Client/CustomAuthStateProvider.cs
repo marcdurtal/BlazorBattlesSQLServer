815d7c6c069ba6b0f96ef8d2a567046f6ebbfd63
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-08 16:01:28 +0100

    Page Restriction with the [Authorize] Attribute

--- BlazorBattlesSQLServer/Client/CustomAuthStateProvider.cs
+++ BlazorBattlesSQLServer/Client/CustomAuthStateProvider.cs
@@ -12,7 +12,7 @@ namespace BlazorBattlesSQLServer.Client
     {
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            //return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
             var identity = new ClaimsIdentity(
                 new[]
                 {
