e16eb2b5b0f8b1290f15ad592d4c7756b82b0db5
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-08 13:30:40 +0100

    User Login Form with Validation

---
+++ BlazorBattlesSQLServer/Shared/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorBattlesSQLServer.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter a Username.")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
