1535ed3d34d751a5b9b7c5f42eb1f9f680ecc2ca
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-08 20:47:26 +0100

    Receive Units from the SQLServer Database

--- BlazorBattlesSQLServer/Server/Controllers/UnitController.cs
+++ BlazorBattlesSQLServer/Server/Controllers/UnitController.cs
@@ -1,5 +1,7 @@
-﻿using BlazorBattlesSQLServer.Shared;
+﻿using BlazorBattlesSQLServer.Server.Data;
+using BlazorBattlesSQLServer.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +13,25 @@ namespace BlazorBattlesSQLServer.Server.Controllers
     [ApiController]
     public class UnitController : ControllerBase
     {
-        public IList<Unit> Units { get; } = new List<Unit> {
-            new Unit { Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100 },
-            new Unit { Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150 },
-            new Unit { Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200 }
-        };
+        private readonly DataContext _context;
+
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        //public IList<Unit> Units { get; } = new List<Unit> {
+        //    new Unit { Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100 },
+        //    new Unit { Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150 },
+        //    new Unit { Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200 }
+        //};
 
         [HttpGet]
-        public IActionResult GetUnits()
+        public async Task<IActionResult> GetUnits()
         {
-            return Ok(Units);
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
         }
     }
 }
