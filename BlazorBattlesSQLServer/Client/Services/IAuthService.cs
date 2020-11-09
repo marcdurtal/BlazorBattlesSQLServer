63ddd5f81972418df36d7bed2805f4a9644465ba
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-09 15:34:56 +0100

    Register a User on the Client

---
+++ BlazorBattlesSQLServer/Client/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorBattlesSQLServer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattlesSQLServer.Client.Services
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<int>> Register(UserRegister request);
+    }
+
+}
