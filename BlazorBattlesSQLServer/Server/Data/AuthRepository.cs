aab718607d70fda52968b71d9d781802a3d41878
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-09 13:04:18 +0100

    “User already exists.”

--- BlazorBattlesSQLServer/Server/Data/AuthRepository.cs
+++ BlazorBattlesSQLServer/Server/Data/AuthRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorBattlesSQLServer.Shared;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,13 @@ namespace BlazorBattlesSQLServer.Server.Data
 
         public  async Task<int> Register(User user, string password)
         {
+
+            if (await UserExists(user.Email))
+            {
+                return -1;
+            }
+
+
             CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.PasswordHash = passwordHash;
@@ -34,9 +42,13 @@ namespace BlazorBattlesSQLServer.Server.Data
             return user.Id;
         }
 
-        public Task<bool> UserExists(string email)
+        public async  Task<bool> UserExists(string email)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower()))
+            {
+                return true;
+            }
+            return false;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
