74bae9b632064f185e17c83a94d5d5da0454dccc
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-10 10:19:48 +0100

    Build UserUnits on the Server with the UserUnitController

---
+++ BlazorBattlesSQLServer/Server/Controllers/UserUnitController.cs
@@ -0,0 +1,50 @@
+﻿using BlazorBattlesSQLServer.Server.Data;
+using BlazorBattlesSQLServer.Server.Services;
+using BlazorBattlesSQLServer.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattlesSQLServer.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserUnitController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUtilityService _utilityService;
+
+        public UserUnitController(DataContext context, IUtilityService utilityService)
+        {
+            _context = context;
+            _utilityService = utilityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuildUserUnit([FromBody] int unitId)
+        {
+            var unit = await _context.Units.FirstOrDefaultAsync<Unit>(u => u.Id == unitId);
+            var user = await _utilityService.GetUser();
+            if (user.Bananas < unit.BananaCost)
+            {
+                return BadRequest("Not enough bananas!");
+            }
+
+            user.Bananas -= unit.BananaCost;
+
+            UserUnit newUserUnit = new UserUnit
+            {
+                UnitId = unit.Id,
+                UserId = user.Id,
+                HitPoints = unit.HitPoints
+            };
+
+            await _context.UserUnits.AddAsync(newUserUnit);
+            await _context.SaveChangesAsync();
+            return Ok(newUserUnit);
+        }
+    }
+}
