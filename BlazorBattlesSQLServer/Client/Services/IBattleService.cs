20b2b2e531042588250f0cf34496fe292805a42c
Philippe Germain-Bonne <PhilippeGermainBonne@hotmail.com>
2020-11-10 17:17:59 +0100

    Fight Battles in Blazor WebAssembly

---
+++ BlazorBattlesSQLServer/Client/Services/IBattleService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorBattlesSQLServer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattlesSQLServer.Client.Services
+{
+    public interface IBattleService
+    {
+        Task<BattleResult> StartBattle(int opponentId);
+    }
+}
